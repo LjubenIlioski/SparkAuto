40fa9f1a720bc69325fee73f3087ba9f4ea5ae23
LjubenIlioski <49167175+LjubenIlioski@users.noreply.github.com>
2021-04-22 22:54:49 +0200

    Created Razor Page Aplication, Created Service Types, Created CRUD operations,

---
+++ SparkAuto/SparkAuto/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SparkAuto.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<ServiceType> ServiceType { get; set; }
+
+    }
+}
