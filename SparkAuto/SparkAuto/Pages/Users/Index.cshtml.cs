ded450cfba7406ee7fb74571a5e87860217093bf
LjubenIlioski <49167175+LjubenIlioski@users.noreply.github.com>
2021-04-24 21:10:28 +0200

    Implemented Search in Users, Edit user and Delete User

--- SparkAuto/SparkAuto/Pages/Users/Index.cshtml.cs
+++ SparkAuto/SparkAuto/Pages/Users/Index.cshtml.cs
@@ -26,7 +26,8 @@ namespace SparkAuto.Pages.Users
         [BindProperty]
         public UsersListViewModel UsersListVM { get; set; }
 
-        public async Task<IActionResult> OnGet(int productPage=1)
+        public async Task<IActionResult> OnGet(int productPage = 1, string searchEmail = null, string searchName = null,
+            string searchPhone = null)
         {
             UsersListVM = new UsersListViewModel()
             {
@@ -36,6 +37,44 @@ namespace SparkAuto.Pages.Users
 
             StringBuilder param = new StringBuilder();
             param.Append("/Users?productPage=:");
+
+            param.Append("&searcName=");
+            if (searchName != null)
+            {
+                param.Append(searchName);
+            }
+
+            param.Append("&searchEmail=");
+            if (searchEmail != null)
+            {
+                param.Append(searchEmail);
+            }
+
+            param.Append("&searchPhone=");
+            if (searchPhone != null)
+            {
+                param.Append(searchPhone);
+            }
+
+            if (searchEmail != null)
+            {
+                UsersListVM.ApplicationUserList = await _db.ApplicationUser.Where(u => u.Email.ToLower().Contains(searchEmail.ToLower())).ToListAsync();
+            }
+            else
+            {
+                if (searchName != null)
+                {
+                    UsersListVM.ApplicationUserList = await _db.ApplicationUser.Where(u => u.Name.ToLower().Contains(searchName.ToLower())).ToListAsync();
+                }
+                else
+                {
+                    if (searchPhone != null)
+                    {
+                        UsersListVM.ApplicationUserList = await _db.ApplicationUser.Where(u => u.PhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToListAsync();
+                    }
+                }
+            }
+
 
             var count = UsersListVM.ApplicationUserList.Count;
             UsersListVM.PagingInfo = new PagingInfo
