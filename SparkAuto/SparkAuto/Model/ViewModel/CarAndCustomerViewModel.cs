bf9908da0af9308c147d9ac896e7aaf20ff8375d
LjubenIlioski <49167175+LjubenIlioski@users.noreply.github.com>
2021-04-25 23:06:30 +0200

    Implemented Car CRUD views and functionality, statred with service implementation,

---
+++ SparkAuto/SparkAuto/Model/ViewModel/CarAndCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Model.ViewModel
+{
+    public class CarAndCustomerViewModel
+    {
+
+        public ApplicationUser UserObj { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+        public string StatusMesage { get; set; }
+    }
+}
