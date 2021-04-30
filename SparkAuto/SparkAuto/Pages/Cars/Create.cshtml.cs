197cecb524c04306edbf1becc32e3111de3c217a
LjubenIlioski <49167175+LjubenIlioski@users.noreply.github.com>
2021-04-30 22:51:19 +0200

    Added authorization to some pages, edited layout page

--- SparkAuto/SparkAuto/Pages/Cars/Create.cshtml.cs
+++ SparkAuto/SparkAuto/Pages/Cars/Create.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SparkAuto.Data;
@@ -10,8 +11,10 @@ using SparkAuto.Model;
 
 namespace SparkAuto.Pages.Cars
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
+       
         private readonly ApplicationDbContext _db;
 
         [BindProperty]
