3c494716a7decd71f48011255a5b53587e7fed77
LjubenIlioski <49167175+LjubenIlioski@users.noreply.github.com>
2021-05-01 15:50:11 +0200

    Created Initializer for the DB , Chenged some view in Login Partial, Worked with view Comoponents

--- SparkAuto/SparkAuto/Startup.cs
+++ SparkAuto/SparkAuto/Startup.cs
@@ -76,12 +76,14 @@ namespace SparkAuto
                 options.Cookie.IsEssential = true;
             });
 
+            services.AddScoped<IDbInitializer, DbInitializer>();
+
             // IDK why :D 
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -94,7 +96,7 @@ namespace SparkAuto
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            dbInitializer.Initialize();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
